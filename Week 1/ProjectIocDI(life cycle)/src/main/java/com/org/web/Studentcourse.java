d2ff5f6ed9dd8303f3f03442247a41ec579371d4
KrishnaPrabha13 <86238794+KrishnaPrabha13@users.noreply.github.com>
2021-07-23 08:04:48 +0530

    Add files via upload

---
+++ Week 1/ProjectIocDI(life cycle)/src/main/java/com/org/web/Studentcourse.java
@@ -0,0 +1,15 @@
+package com.org.web;
+
+public class Studentcourse 
+{
+	private String course;
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	
+}
